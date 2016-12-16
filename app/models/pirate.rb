7c9dd022a39c4d1b46944fc52c394deb86fa2787
Rob Hitt <robhitt@gmail.com>
2016-12-16 15:16:37 -0500

    done

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,16 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+
+  PIRATES = []
+
+  def initialize(pirate_hash)
+    @name = pirate_hash[:name]
+    @weight = pirate_hash[:weight]
+    @height = pirate_hash[:height]
+    PIRATES << self
+  end
+
+  def self.all
+    PIRATES
+  end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,21 @@
 class Ship
+  #he ship class should have name, type, and booty attributes, as well as a class method .all that returns all the ships and a class method .clear that deletes all ships.
+  attr_accessor :name, :type, :booty
+
+  SHIPS = []
+
+  def initialize(ship_hash)
+    @name = ship_hash[:name]
+    @type = ship_hash[:type]
+    @booty = ship_hash[:booty]
+    SHIPS << self
+  end
+
+  def self.all
+    SHIPS
+  end
+
+  def self.clear
+    SHIPS.clear
+  end
 end
